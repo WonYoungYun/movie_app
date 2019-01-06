ae02d051a1050d20d712da9f2ada09e92dd997c5
WonYoungYun <asmar20288@naver.com>
2019-01-06 17:22:08 +0900

    model

--- js/models/ActorModel.js
+++ js/models/ActorModel.js
@@ -1,31 +1,35 @@
 const KEY = "9e8b999fb271009b38df5afcdefd5c35";
 
 export default {
-    list() {
-        return new Promise(res => {
-            setTimeout(() => {
-                res(this.getActorList())
-            }, 200)
-        })
-    },
+  list() {
+    return new Promise(res => {
+      setTimeout(() => {
+        res(this.getActorList());
+      }, 200);
+    });
+  },
 
-    getActorList() {
-        const refinedData = [];
-        return fetch(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?key=${KEY}`).then(res => {
-            return res.json()
-        }).then(json => {
-            const data = json.peopleListResult.peopleList
-            Array.from(data).forEach(element => {
-                refinedData.push(this.refindingData(element))
-            })
-            return refinedData
-        })
-    },
+  getActorList() {
+    const refinedData = [];
+    return fetch(
+      `https://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.json?key=${KEY}`
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        const data = json.peopleListResult.peopleList;
+        Array.from(data).forEach(element => {
+          refinedData.push(this.refindingData(element));
+        });
+        return refinedData;
+      });
+  },
 
-    refindingData(item) {
-        return {
-            peopleNm: item.peopleNm,
-            peopleNmEn: item.peopleNmEn
-        }
-    }
-}
+  refindingData(item) {
+    return {
+      peopleNm: item.peopleNm,
+      peopleNmEn: item.peopleNmEn
+    };
+  }
+};
--- js/models/SearchModel.js
+++ js/models/SearchModel.js
@@ -1,53 +1,51 @@
 const KEY = "9e8b999fb271009b38df5afcdefd5c35";
 
 const MESSAGES = {
-    no_info: "정보가 없습니다."
+  no_info: "정보가 없습니다."
 };
 
-
-
 export default {
-    list(query) {
-        const list = this.getMovie(query);
-        return list;
-    },
+  list(query) {
+    const list = this.getMovie(query);
+    return list;
+  },
 
-    getMovie(query) {
-        const refineData = [];
-        return fetch(
-                `http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${KEY}&movieNm=${query}`
-            )
-            .then(res => {
-                return res.json();
-            })
-            .then(json => {
-                const data = json.movieListResult.movieList;
-                data.forEach(element => {
-                    refineData.push(this.refindingData(element));
-                });
-                return refineData;
-            });
-    },
-    refindingData(item) {
-        const nationAlt = item.nationAlt.length ? item.nationAlt : MESSAGES.no_info;
-        const openDt = item.openDt.length ? item.openDt : MESSAGES.no_info;
-        const directors = item.directors.length ?
-            this.refindingDirectors(item.directors) :
-            MESSAGES.no_info;
-        return {
-            movieNm: item.movieNm,
-            movieNmEn: item.movieNmEn,
-            genreAlt: item.genreAlt,
-            nationAlt,
-            openDt,
-            directors
-        };
-    },
-    refindingDirectors(directors) {
-        const movieDirectors = [];
-        directors.forEach(element => {
-            movieDirectors.push(element.peopleNm);
+  getMovie(query) {
+    const refineData = [];
+    return fetch(
+      `https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=${KEY}&movieNm=${query}`
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(json => {
+        const data = json.movieListResult.movieList;
+        data.forEach(element => {
+          refineData.push(this.refindingData(element));
         });
-        return movieDirectors;
-    }
+        return refineData;
+      });
+  },
+  refindingData(item) {
+    const nationAlt = item.nationAlt.length ? item.nationAlt : MESSAGES.no_info;
+    const openDt = item.openDt.length ? item.openDt : MESSAGES.no_info;
+    const directors = item.directors.length
+      ? this.refindingDirectors(item.directors)
+      : MESSAGES.no_info;
+    return {
+      movieNm: item.movieNm,
+      movieNmEn: item.movieNmEn,
+      genreAlt: item.genreAlt,
+      nationAlt,
+      openDt,
+      directors
+    };
+  },
+  refindingDirectors(directors) {
+    const movieDirectors = [];
+    directors.forEach(element => {
+      movieDirectors.push(element.peopleNm);
+    });
+    return movieDirectors;
+  }
 };
